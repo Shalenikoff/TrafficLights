72de39de262df32415f428c3a9662d0c739a5310
Kirill Shalenikov <KShalen@gmail.com>
2023-01-07 23:57:48 +0300

    Made outlets for each element in area

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightLabel: UILabel!
+    @IBOutlet var yellowLightLabel: UILabel!
+    @IBOutlet var greenLightLabel: UILabel!
+    @IBOutlet var mainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
